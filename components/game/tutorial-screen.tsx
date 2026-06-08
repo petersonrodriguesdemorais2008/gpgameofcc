b15382e1f6677a57aca350e9d732713b7954e10c
petersonrodriguesdemorais2008 <petersonrodriguesdemorais2008@gmail.com>
2026-06-07 21:07:27 -0300

    Fix background image syntax in tutorial screen

--- components/game/tutorial-screen.tsx
+++ components/game/tutorial-screen.tsx
@@ -620,7 +620,7 @@ function MasterSelectPhase({ playerName, onSelect, selectedMaster, confirmed }: 
       {/* Ruído de fundo sutil */}
       <div style={{
         position: "absolute", inset: 0, opacity: 0.025, pointerEvents: "none",
-        backgroundImage: "url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.85' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E")",
+        backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.85' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E")`,
         backgroundRepeat: "repeat", backgroundSize: "200px",
       }} />
 
