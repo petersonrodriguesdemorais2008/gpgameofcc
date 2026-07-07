8a0604e2c4f8eb0a59fd62fb06ded95fd5e4e329
petersonrodriguesdemorais2008 <petersonrodriguesdemorais2008@gmail.com>
2026-07-07 19:50:00 -0300

    Update bot life to use startingLP variable

--- components/game/duel-screen.tsx
+++ components/game/duel-screen.tsx
@@ -4587,7 +4587,7 @@ export function DuelScreen({ mode, onBack, onWin, draftDeck, draftDifficulty, st
       hand: botHand,
       deck: botRemaining,
       tap: activeBotDeck.tapCards ? [...activeBotDeck.tapCards] : [],
-      life: 50,
+      life: startingLP,
       unitZone: [null, null, null, null],
       functionZone: [null, null, null, null],
       scenarioZone: null,
